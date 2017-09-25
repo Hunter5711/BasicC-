9c432c3f4eb4aee616c73a1cb661d9ae714e92c5
= <=>
2017-09-25 18:45:54 +0530

    provide function for searching any  element in Array

--- BeginC++/ArrayBsearch.cpp
+++ BeginC++/ArrayBsearch.cpp
@@ -18,6 +18,7 @@ cout<<endl;
 int main()
 {
 	int a[] = {2,41,5,8,1,6,71,21,19,30};
+	int key;
 	int asize = sizeof(a)/sizeof(a[0]);
 	print(a,asize);
 	//sorting the array elements
@@ -25,11 +26,13 @@ int main()
 cout<<"Array after sorting "<<endl;
 print(a, asize);
 cout<<endl;
-
-if (binary_search(a, a + 10, 2)) 
-       cout << "\nElement found in the array"<<endl;
+cout<<"Searching a key in array "<<endl;
+cout<<"Enter a key to be searched "<<endl;
+cin>>key;
+if (binary_search(a, a + 10, key)) 
+       cout << "\nElement"<<"\t" <<key<<"found in the array"<<endl;
     else
-       cout << "\nElement not found in the arra"<<endl;
+       cout << "\nElement"<<"\t"<<key<<" not found in the arra"<<endl;
 return 0;
 
 }
